971782db90783926fd58c13fb9a6835c846f6fbb
Joshua Nanninga <jmarknanninga@gmail.com>
2025-02-10 10:45:17 -0600

    fixed main.css

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Egg & Milk Exchange',
+        short_name: 'EggMilk',
+        description: 'Local exchange platform for eggs and dairy products',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
